247a4fedfb6a7dcf666c1c09ac4967b7d616be6a
Bernard <bernard.guo1994@gmail.com>
2019-08-22 19:42:25 +1000

    set app for heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const councils = require('./routes/councils');
 const councilAreas = require('./routes/councilAreas');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://localhost/geodata', { useNewUrlParser: true })
     .then(async () => {
@@ -14,5 +15,5 @@ app.use(express.json());
 app.use('/api/councils', councils);
 app.use('/api/council-areas', councilAreas);
 
-const port = process.env.PORT || 3000;
+
 app.listen(port, () => console.log(`Listening on port ${port}...`));
