92fed457d7fb86fb8499edee218eb86c547cdf0b
Bernard <bernard.guo1994@gmail.com>
2019-09-22 01:51:07 +1000

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const areas = require('./routes/areas');
 const express = require('express');
 const config = require('config');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
 mongoose.connect(config.db, { useNewUrlParser: true })
@@ -13,13 +14,7 @@ mongoose.connect(config.db, { useNewUrlParser: true })
 
 app.use(express.json());
 app.use('/api/areas', areas);
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors());
 
 //console.log(process.env.NODE_ENV);
 
