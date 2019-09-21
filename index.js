96a7d6c916c886d85b31b646ec33d6795e179b57
Bernard <bernard.guo1994@gmail.com>
2019-09-22 01:44:33 +1000

    add res header

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ mongoose.connect(config.db, { useNewUrlParser: true })
 
 app.use(express.json());
 app.use('/api/areas', areas);
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 //console.log(process.env.NODE_ENV);
 
