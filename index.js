8b8e54bf69817905a3b747858c5f01e46ef551cf
Bernard <bernard.guo1994@gmail.com>
2019-10-07 21:33:59 +1100

    add path

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const config = require('config');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 mongoose.connect(config.db, { useNewUrlParser: true })
